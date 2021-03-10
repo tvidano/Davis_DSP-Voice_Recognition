bc690a12fdb4742b274adf9184c60e256fe87d38
tvidano <61514829+tvidano@users.noreply.github.com>
2021-03-09 20:44:55 -0800

    Add files from canvas.

---
+++ main.m
@@ -0,0 +1,9 @@
+%{
+Main script for MATLAB Project voice_recognition
+
+Authors: Tanuj Kalakuntla, Trevor Vidano
+Date: 03/20/2021
+
+In accordance with the completion of UC Davis' EEC 201: Digital Signal
+Processing.
+%}
