5347bc9478b2f64fc7d661e5085de9bc5422ba3a
tvidano <61514829+tvidano@users.noreply.github.com>
2021-03-10 12:01:44 -0800

    New function for converting audio to MFCC.

--- main.m
+++ main.m
@@ -7,3 +7,5 @@ Date: 03/20/2021
 In accordance with the completion of UC Davis' EEC 201: Digital Signal
 Processing.
 %}
+clc; clearvars; close all;
+speechpreprocess('Data/Test_Data/s1.wav',true)
---
+++ speechpreprocess.m
@@ -0,0 +1,56 @@
+function [] = speechpreprocess(path, plots)
+%SPEACHPREPROCESS Reads a sound file and converts to MFCC sequence.
+%
+% Inputs:           path    string, filepath to sound file
+%                   plots   logical, plot the signal in time domain
+%
+% Outputs:
+%
+% Usage:
+
+% check valid data types
+assert(isstring(path)||iscellstr(path)||ischar(path),...
+    ['path variable is not type string array, cell array of character',...
+    'vectors, or character array.'])
+assert(islogical(plots),'plot variable is not type logical.')
+
+[x, fs] = audioread(path);
+
+% Peak Normalization
+if size(x,1) == 1
+    peak = max(abs(x));
+    x = x/peak;
+elseif size(x,2) == 2
+    % Convert Stereo to Mono
+    xMono = x(:,1) + x(:,2);
+    peak = max(abs(xMono));
+    xMono = xMono/peak;
+    
+    peakL = max(abs(x(:,1)));
+    peakR = max(abs(x(:,2)));
+    stereoPeak = max([peakL, peakR]);
+    
+    xMono = xMono*stereoPeak;
+    x = xMono;
+elseif size(x,2) > 2
+    error('speachpreprocess:tooManyChannels',...
+        'Error. \nAudio file must contain 1 or 2 channels, not %i',...
+        size(x,2));
+end
+
+% Plot audio in time domain
+if plots
+    lenX = length(x);
+    t = 0:1/fs:lenX/fs-1/fs;
+    
+    figure;
+    plot(t,x');
+    title('Audio File in Time Domain');
+    xlabel('Time [s]');
+    ylabel('Amplitude');
+    grid on;
+    
+    numMs = 256/fs*1000;
+    fprintf('There are %.2f ms of speech in 256 samples.\n',numMs);
+end
+end
